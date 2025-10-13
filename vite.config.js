782e45ca4a68f5f95513908d6eb0dc7738a69842
Norki10 <norki10@norkarym.com>
2025-10-13 18:32:54 +0200

    NORKKI DEV

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
-// OFICIAL_NORKARYM/vite.config.js (Versió Definitiva i Correcta)
+// OFICIAL_NORKARYM/vite.config.js (Versió Actualitzada)
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // 1. Definim on es troba el codi font del nostre frontend.
   root: resolve(__dirname, 'CLIENT'),
-
-  // 2. Configurem el procés de 'build' per a producció.
   build: {
-    // La carpeta on es generarà la versió final es dirà 'dist' i estarà a l'arrel del projecte.
     outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
-      // Definim explícitament cada pàgina HTML de la nostra aplicació.
-      // Això és més robust que buscar fitxers automàticament.
       input: {
+        // Pàgina principal (la que ja teníem)
         main: resolve(__dirname, 'CLIENT/index.html'),
-        // Quan creeu més pàgines, les afegireu aquí. Per exemple:
-        // login: resolve(__dirname, 'CLIENT/src/pages/login.html'),
+
+        // Afegim la nova pàgina d'inscripció.
+        // L'arxiu físic ha d'estar a CLIENT/src/pages/inscripcio.html
+        inscripcio: resolve(__dirname, 'CLIENT/src/pages/inscripcio.html'),
       },
     },
   },
