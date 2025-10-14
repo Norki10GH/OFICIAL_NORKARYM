dceb3a9c0c765e579715f1ca996ca647a7725495
Norki10 <norki10@norkarym.com>
2025-10-14 03:43:11 +0200

    Norki up to firabase

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,34 @@
-// functions/index.js (Actualitzat a la sintaxi v2)
+// functions/index.js (Corregido)
 
-// Importacions per a les funcions de Firebase (nova sintaxi v2)
-import {log} from "firebase-functions/logger";
-import {onRequest} from "firebase-functions/v2/https";
-import {setGlobalOptions} from "firebase-functions/v2";
-
+import { onRequest } from "firebase-functions/v2/https";
+import { setGlobalOptions } from "firebase-functions/v2";
 import admin from "firebase-admin";
+import cors from "cors";
+
 import { addAdmin } from "./src/controllers/adminController.js";
-// Mantenim l'exemple anterior per si el necessites
-// import { laTevaFuncio } from "./src/controllers/elTeuController.js";
+import { registerNewUser } from "./src/controllers/userController.js";
 
-// --- INICIALITZACIÓ I CONFIGURACIÓ GLOBAL ---
-
-// Inicialitza l'SDK d'Admin de Firebase. Només es fa una vegada.
+// Inicializa l'SDK d'Admin de Firebase.
 admin.initializeApp();
 
-// Estableix la regió per a totes les funcions desplegades des d'aquest fitxer.
-// És una bona pràctica per mantenir les teves funcions a prop dels teus usuaris.
-setGlobalOptions({region: "europe-west1"});
+// Establece la región para todas las funciones.
+setGlobalOptions({ region: "europe-west1" });
 
-// Crea un gestor de CORS que podem reutilitzar a totes les funcions HTTP.
-const corsHandler = cors({origin: true});
+// Crea un gestor de CORS reutilizable.
+const corsHandler = cors({ origin: true });
 
+// --- ENDPOINTS DE L'API ---
 
-// --- DEFINICIÓ DELS ENDPOINTS DE L'API ---
-
-// Endpoint per afegir un nou administrador
-export const apiAddAdmin = functions.region("europe-west1").https.onRequest((req, res) => {
-  corsHandler(req, res, () => {
-    // Aquí podríem afegir un middleware de verificació d'autenticació en el futur
-    addAdmin(req, res);
+// Endpoint para añadir un nuevo administrador
+export const apiAddAdmin = onRequest((req, res) => {
+  corsHandler(req, res, async () => {
+    await addAdmin(req, res);
   });
 });
 
-// Exemple:
-// export const elTeuEndpoint = functions.region("europe-west1").https.onRequest(...)
+// Endpoint para registrar un nuevo usuario
+export const apiRegisterNewUser = onRequest((req, res) => {
+  corsHandler(req, res, async () => {
+    await registerNewUser(req, res);
+  });
+});
