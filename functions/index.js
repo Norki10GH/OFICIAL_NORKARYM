c38db0bee69ba8b4a6fd0861c3f6c764a5fac85c
norki10 <norki10@norkarym.com>
2025-10-16 01:09:43 +0000

    Norki up to firabase

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 // Contenido para el archivo: functions/index.js
 
+// FORÇAR REDESPLEGAMENT
 import { onRequest } from "firebase-functions/v2/https";
 import { setGlobalOptions } from "firebase-functions/v2";
 import admin from "firebase-admin";
