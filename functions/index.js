41c37ee4c45e0a7e382b1f29bf0c0f407e3e1592
Norki10 <norki10@norkarym.com>
2025-10-17 17:30:45 +0200

    Norki up to firabase

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,21 @@
 // functions/index.js
 const { onRequest } = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
-const api = require("./src/api");
+const express = require("express");
+const apiApp = require("./src/api"); // Importem la nostra app d'API
 
-// Inicialitzem Firebase Admin (només una vegada)
+// Inicialitzem Firebase Admin
 admin.initializeApp();
 
-// Exportem l'app d'Express com una única Cloud Function
-exports.api = onRequest({ region: "europe-west1" }, api);
+// Creem una app principal d'Express que actuarà com a enrutador
+const mainApp = express();
+
+// Aquesta és la línia clau:
+// Li diem a l'app principal que totes les rutes definides a 'api.js'
+// han d'estar sota el prefix '/api'.
+// Ara, quan arribi una petició a /api/registrarAdmin, buscarà /registrarAdmin dins de apiApp.
+mainApp.use('/api', apiApp);
+
+// Exportem l'app principal d'Express com la nostra Cloud Function.
+// El nom "api" aquí ha de coincidir amb el de firebase.json
+exports.api = onRequest({ region: "europe-west1" }, mainApp);
