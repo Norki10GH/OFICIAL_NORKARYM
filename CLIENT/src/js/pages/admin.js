4c39697cfad37781ae287e60bbabadaa234c900e
Norki10 <norki10@norkarym.com>
2025-10-17 23:19:48 +0200

    Norki up to firabase

---
+++ CLIENT/src/js/pages/admin.js
@@ -0,0 +1,11 @@
+// client/src/js/pages/admin.js
+
+export function adminPage() {
+  console.log("S'ha carregat la lògica de la pàgina d'administració.");
+
+  // Aquí anirà tota la lògica específica del panell d'administració:
+  // - Gestió de formularis (nou admin, assignar rols, etc.)
+  // - Càrrega de dades a les taules (administradors, productes, auditoria)
+  // - Gestió de modals (editar, veure versions, etc.)
+  // - Interacció amb els endpoints de l'API del backend
+}
